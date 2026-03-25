240937287041063f34babf76f5a1362e8eeb1eb4
Callum Skeet <cskeet@fastmailteam.com>
2026-03-25 18:30:31 +1100

    Add prettier and stylelint

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+export default {
+    singleQuote: true,
+    tabWidth: 4,
+    quoteProps: 'consistent',
+    trailingComma: 'all',
+    plugins: ['@shopify/prettier-plugin-liquid'],
+    overrides: [
+        {
+            files: ['*.html'],
+            options: {
+                tabWidth: 2,
+                printWidth: 1000,
+            },
+        },
+    ],
+};
