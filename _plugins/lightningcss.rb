08846a5764e93360efd6dcef291b97ef0c9b3408
Callum Skeet <cskeet@fastmailteam.com>
2026-04-14 11:15:23 +1000

    Add build step for js/css
    Replace Sass with lightningcss and add esbuild for JS bundling

---
+++ _plugins/lightningcss.rb
@@ -0,0 +1,36 @@
+require "open3"
+require "fileutils"
+
+Jekyll::Hooks.register :site, :post_write do |site|
+  css_dir = File.join(site.dest, "css")
+  next unless Dir.exist?(css_dir)
+
+  targets = site.config.dig("lightningcss", "targets")
+  unless targets
+    raise "No browser target set for lightningcss"
+  end
+
+  css_files = Dir.glob(File.join(css_dir, "*.css"))
+
+  css_files.each do |file|
+    cmd = [
+      "npx", "lightningcss",
+      "--minify",
+      "--bundle",
+      "--targets", targets,
+      file,
+      "-o", file
+    ]
+
+    _out, err, status = Open3.capture3(*cmd)
+
+    unless status.success?
+      raise "lightningcss failed on #{file}:\n#{err}"
+    end
+  end
+
+  module_dir = File.join(css_dir, "module")
+  FileUtils.rm_rf(module_dir) if Dir.exist?(module_dir)
+
+  Jekyll.logger.info "LightningCSS:", "processed #{css_files.size} file(s)"
+end
